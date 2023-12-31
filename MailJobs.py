8374d825e9297dd56a9aa6452673b0f0d982e411
cneely33 <cneely33@gmail.com>
2023-12-31 09:07:08 -0600

    Update MailJobs.py

--- MailJobs.py
+++ MailJobs.py
@@ -2,23 +2,27 @@ import psycopg2
 import pandas as pd
 import numpy as np
 
-#### Connect to Postgresql server ####
-conn_string = "host='localhost' dbname='Spyder' user='postgres' password='DiabloOverwatchEnd'"
+from secrets import secrets
+from os import getenv
 
-print("Connecting to database {0}".format(conn_string))
+secrets.load_env_vars()
+
+#### Connect to local Postgresql server ####
+password = getenv('Local_Postgres')
+conn_string = "host='localhost' dbname='Spyder' user='postgres' password={password}".format(password=password)
 
 conn = psycopg2.connect(conn_string)
-
 cursor = conn.cursor()
-print("Connected!\n")
+print("Connected\n")
 
 ### SQL Statement ###
-postgres = "SELECT * FROM public.policejobs"
+postgres = "SELECT * FROM jobs"
 
 df = pd.read_sql(postgres, conn)
 
+conn.close()
+
 ### Select a random row from dataframe ###
-#TODO change this to make the SQL statement pick a random row
 row = np.random.choice(df.index.values, 1)
 
 df_1 = df.iloc[row]
@@ -44,44 +48,11 @@ smtpObj.ehlo()
 smtpObj.starttls()
     
     #Login to Gmail account
-smtpObj.login('BeTheChangeJohn@gmail.com', 'DiabloOverwatchEnd')
+smtpObj.login('BeTheChangeJohn@gmail.com', getenv('eicher_email'))
     
     #send a test email
 smtpObj.sendmail('BeTheChangeJohn@gmail.com', '4074544725@txt.att.net',
-                    'Sarah truly can F**K THA POLICE when you\
- work for Law Enforcement! Apply\
- for {0} at {1} in lovely {2} \
- with link {3}'.format(title, company, location, link))
+                    'Apply for {0} at {1} in lovely {2} with link {3}'.format(title, company, location, link))
 
-smtpObj.sendmail('BeTheChangeJohn@gmail.com', '4074544725@txt.att.net',
-                    'Sarah truly can FUCK THA POLICE when you\
- work for Law Enforcement! Apply\
- for {0} at {1} in lovely {2} \
- with link {3}'.format(title, company, location, link))
-
-smtpObj.sendmail('BeTheChangeJohn@gmail.com', 'cneely33@gmail.com',
-                    'Sarah truly can FUCK THA POLICE when you\
- work for Law Enforcement! Apply\
- for {0} at {1} in lovely {2} \
- with link {3}'.format(title, company, location, link))
-
-smtpObj.sendmail('BeTheChangeJohn@gmail.com', '2258038146@messaging.sprintpcs.com',
-                    'Test \
- work for Law Enforcement! Apply\
- for {0} at {1} in lovely {2} \
- with link {3}'.format(title, company, location, link))
-
-#print('Subject: Law Enforcement Jobs! \n Be The Change You Want to See! \
-#Apply for a {0} at {1} in lovely {2} \
-#with this link {3}'.format(title, company, location, link))
-
-#Eicher
-#4074544725@txt.att.net                     
-#dupes
-#3372902976@txt.att.net  
-    #close the SMTP connetion
 smtpObj.quit()
 
-
-
-conn.close()
